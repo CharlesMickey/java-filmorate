666f9b21411857e62cfe0727357c5575d77fb472
Artur Chukhnenko <CharlesMickey.ru@yandex.ru>
2023-09-04 16:10:32 +0300

    Feat: добавить новый функционал

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,27 @@
+package ru.yandex.practicum.filmorate.model;
+
+import java.time.LocalDate;
+import javax.validation.constraints.*;
+import lombok.Data;
+import org.springframework.validation.annotation.Validated;
+
+@Data
+@Validated
+public class User {
+
+  private int id;
+
+  @NotBlank(message = "Email обязательно поле")
+  @Email(message = "Не верный формат email")
+  private String email;
+
+  @NotBlank(message = "Логин - обязательное поле")
+  @Pattern(regexp = "\\S+", message = "В логине не должно быть пробелов")
+  private String login;
+
+  private String name;
+
+  @NotNull(message = "Дата рождение – обязательное поле")
+  @Past(message = "Дата рождения не может быть в будущем")
+  private LocalDate birthday;
+}
